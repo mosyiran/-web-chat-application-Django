a3e1c9070889226a5a3cdd6970facabee087ba1a
mosyiran <eng.goudarzi@outlook.com>
2024-11-15 18:27:08 +0330

    create pages and link pages

---
+++ chatproject/chat/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views import View
+
+
+# Create your views here.
+
+
+class Main(View):
+    def get(self, request):
+        return render(request=request, template_name="chat/main.html")
+
+
+class Login(View):
+    def get(self, request):
+        return render(request=request, template_name="chat/login.html")
+
+class Register(View):
+    def get(self, request):
+        return render(request=request, template_name="chat/register.html")
+
+class Logout(View):
+    def get(self, request):
+        return render(request=request, template_name="chat/logout.html")
+
+
+class Home(View):
+    def get(self, request):
+        return render(request=request, template_name="chat/home.html")
+
+class ChatPerson(View):
+    def get(self, request):
+        return render(request=request, template_name="chat/chat_person.html")
